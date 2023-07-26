eecc170b1deca4eaf3bdda068c0c9bee3b6bebcd
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2023-07-26 23:25:18 +0800

    passed

--- two_sum.js
+++ two_sum.js
@@ -6,4 +6,30 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 
-asdasdsadsa
+
+var twoSum = function(nums, target) {
+    let map = new Map();
+    
+    let x1 = 0
+    let x2 = 0
+
+    for(let i = 0 ; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if(map.has(complement)) {
+            x1 = map.get(complement)
+            x2 = i
+        }
+        map.set(nums[i], i)
+    }
+
+    let xx = [x1, x2]
+
+    return xx;
+
+};
+
+
+const arr = [2,7,11,15];
+const target = 9;
+console.log(twoSum(arr, target));
