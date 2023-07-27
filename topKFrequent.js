7e6b5149c84c0facefdb8186830c94634854b8b1
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2023-07-27 23:47:19 +0800

    passed

---
+++ topKFrequent.js
@@ -0,0 +1,24 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+*/
+
+const topKFrequent = (nums, k) => {
+
+    const freqMap = new Map()
+    
+    nums.forEach(num => {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1)
+    })
+
+    const sortedNums = Array.from(freqMap.keys()).sort((a, b) => {
+        const freqA = freqMap.get(a);
+        const freqB = freqMap.get(b);
+        return freqB - freqA;
+      });
+    
+      return sortedNums.slice(0, k);
+}
+
+const nums = [1,1,1,2,2,3], k = 1
+
+console.log(topKFrequent(nums, k))
